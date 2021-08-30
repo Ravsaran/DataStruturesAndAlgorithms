814443a490dc1ff4ec1985d566b5a3480a26512d
Ravsaran <ravitejasaranga94@gmail.com>
2021-08-30 17:56:40 -0500

    Create ArrayReverse.java
    reverse the array using till mid element with out using any new array

---
+++ LinearSearch/src/ArrayReverse.java
@@ -0,0 +1,22 @@
+
+public class ArrayReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[] {10,20,30,45,60,80,90,100};
+		int len = array.length-1;
+		
+		int mid = len/2;
+		for(int i=0; i<= mid; i++) {
+			
+			int temp = array[len -i];
+			array[len-i]= array[i];
+			array[i]= temp;
+			
+		}
+		
+		for(int i=0;i<=len;i++)
+			System.out.print(array[i] + ",");
+	}
+
+}
